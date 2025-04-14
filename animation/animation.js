0dd9a52cdc96b676d7c12207e34b9bf238f557c9
Koloina01 <kolomaherison@gmail.com>
2025-04-14 19:40:09 +0300

    fix: fixed the navbar

--- animation/animation.js
+++ animation/animation.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Initialisation de AOS
     AOS.init({
         duration: 800,
         easing: 'ease-in-out',
         once: true
     });
 
-    // Animation de texte
     const text = "Master touch typing with Vanilla Typing – the premier platform to refine your keyboard mastery.";
     const typedTextElement = document.getElementById('typed-text');
     let i = 0;
@@ -20,7 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
             typedTextElement.style.animation = "blinkCursor 0.7s infinite";
         }
     }
-
-    // Démarrer l'animation après un léger délai
     setTimeout(typeWriter, 1000);
+
+    const navbar = document.querySelector(".navbar");
+    if (navbar) {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 50) {
+                navbar.classList.add("blur-navbar");
+            } else {
+                navbar.classList.remove("blur-navbar");
+            }
+        });
+    }
 });
